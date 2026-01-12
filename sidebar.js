a46f128fc4ccfe6140065f598958480fff268c95
citronellaclub-collab <citronellaclub@gmail.com>
2026-01-12 19:58:17 -0300

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll(".sidebar a");
-
-  links.forEach(link => {
-    if (link.href === window.location.href) {
-      link.classList.add("active");
-    }
-  });
-});
 
